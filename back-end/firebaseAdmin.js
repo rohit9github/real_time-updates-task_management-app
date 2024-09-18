995ff5b4ac3e20d6c6d2fb0c6d353c42ccc8cf7d
rohit9github <gohilrohit@gmail.com>
2024-09-19 05:22:19 +0530

    add

--- back-end/firebaseAdmin.js
+++ back-end/firebaseAdmin.js
@@ -1,5 +1,5 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./task-management-ec909-firebase-adminsdk-kugyo-36519e43c4.json");
+const serviceAccount = require("./servicesAction.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
--- front-end/firebase-messaging-sw.jsx
+++ front-end/firebase-messaging-sw.jsx
@@ -1,23 +1,23 @@
-importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging.js');
+// importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js');
+// importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging.js');
 
-firebase.initializeApp({
-  apiKey: "AIzaSyCrqXOnbVe7UE4pHfD2iierm9NwAo2jqjc",
-  authDomain: "task-management-ec909.firebaseapp.com",
-  projectId: "task-management-ec909",
-  storageBucket: "task-management-ec909.appspot.com",
-  messagingSenderId: "690213977727",
-  appId: "1:690213977727:web:07cfc9b44e5320f630894a",
-  measurementId: "G-GMZJ9F7DKT"
-});
+// firebase.initializeApp({
+//   apiKey: "AIzaSyCrqXOnbVe7UE4pHfD2iierm9NwAo2jqjc",
+//   authDomain: "task-management-ec909.firebaseapp.com",
+//   projectId: "task-management-ec909",
+//   storageBucket: "task-management-ec909.appspot.com",
+//   messagingSenderId: "690213977727",
+//   appId: "1:690213977727:web:07cfc9b44e5320f630894a",
+//   measurementId: "G-GMZJ9F7DKT"
+// });
 
-const messaging = firebase.messaging();
+// const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage((payload) => {
-  console.log('Received background message ', payload);
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-  };
-  self.registration.showNotification(notificationTitle, notificationOptions);
-});
+// messaging.onBackgroundMessage((payload) => {
+//   console.log('Received background message ', payload);
+//   const notificationTitle = payload.notification.title;
+//   const notificationOptions = {
+//     body: payload.notification.body,
+//   };
+//   self.registration.showNotification(notificationTitle, notificationOptions);
+// });
